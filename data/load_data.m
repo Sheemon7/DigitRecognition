f846dd0fcef572a822e3a77cf42e7c1006e815cc
Simon Mandlik <simon.mandlik@gmail.com>
2016-12-30 10:45:14 +0100

    removed python code and added matlab code

---
+++ data/load_data.m
@@ -0,0 +1,27 @@
+function [training_data, test_data] = load_data(folder)
+data = {};
+for number = 0:9
+    fpath = fullfile(folder, sprintf('data%d.txt', number));
+    fid=fopen(fpath,'r');
+    res = result_vector(number);
+    while ~feof(fid)
+        [img, ~]=fread(fid,[28 28],'uchar');
+        if isempty(img)
+            break;
+        end
+        img = normalize_matrix(img');
+        data = [data; {img, res}];
+    end
+    fclose(fid);
+end
+
+data = data(randperm(length(data)), :);
+half = 9000;
+training_data = data(1:half, :);
+test_data = data(half+1:end, :);
+end
+
+function vec = result_vector(num)
+vec = zeros(10, 1);
+vec(num + 1) = 1.0;
+end
---
+++ data/load_mnist.m
@@ -0,0 +1,25 @@
+function [training_data, test_data] = load_mnist()
+images = load_mnist_imgs('data\train-images.idx3-ubyte');
+labels = load_mnist_labels('data\train-labels.idx1-ubyte');
+
+training_data = cell(length(labels), 2);
+for n = 1:length(labels)
+    res = result_vector(labels(n));
+    training_data{n, 1} = images(:, n);
+    training_data{n, 2} = res;
+end
+
+images = load_mnist_imgs('data\t10k-images.idx3-ubyte');
+labels = load_mnist_labels('data\t10k-labels.idx1-ubyte');
+
+test_data = cell(length(labels), 2);
+for n = 1:length(labels)
+    test_data{n, 1} = images(:, n);
+    test_data{n, 2} = labels(n);
+end
+end
+
+function vec = result_vector(num)
+vec = zeros(10, 1);
+vec(num + 1) = 1.0;
+end
